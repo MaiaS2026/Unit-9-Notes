d7e1fcf2c2aa5baaabf11d0f33c96fb693922ff4
MaiaS2026 <maia.salgado2026@gbwl.org>
2025-03-07 14:33:53 +0000

    constructor notes

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      // Use the default/no-arg constructors
+      Vehicle v1 = new Vehicle();
+      Car c1 = new Car();
+      // Use the parameterized constructors
+      // ORDER of arguments must match formal params!!!
+      Vehicle v2 = new Vehicle(3, 10.0, "Blue");
+      Car c2 = new Car(4, 65.0, "Navy Blue", 5, "Mercedes Convertible"); 
+
+
    }
 }
